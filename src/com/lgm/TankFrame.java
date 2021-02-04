93c31cf618a3ec8e8f3252fdb4a2bfc0c9ae862f
王昱人 <31147133+291298523@users.noreply.github.com>
2021-02-04 19:56:36 +0800

    添加键盘监听
    手动repaint()，调用paint方法，实现方块的移动

--- src/com/lgm/TankFrame.java
+++ src/com/lgm/TankFrame.java
@@ -1,6 +1,8 @@
 package com.lgm;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -9,12 +11,17 @@ import java.awt.event.WindowEvent;
  * @date:2021/2/4 17:17
  */
 public class TankFrame extends Frame {
+    private int x = 80;
+    private int y = 60;
     public TankFrame() throws HeadlessException {
         this.setTitle("坦克大战");
         this.setSize(800,600);
         this.setBackground(Color.pink);
         this.setResizable(false);
         this.setVisible(true);
+        //添加键盘监听器
+        this.addKeyListener(new MyKeyAdapter());
+        //添加窗口监听器
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -26,6 +33,20 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         System.out.println("..............Graphics is painting.............");
-        g.fillRect(200,200,50,50);
+        g.fillRect(x,y,50,50);
+        x+=40;
+        y+=30;
+    }
+    class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            repaint();
+        }
     }
 }
