b5c52e6baa83417d297fe3add690a7dd9d4ad512
王昱人 <31147133+291298523@users.noreply.github.com>
2021-02-04 15:20:48 +0800

    new一个窗口
    创建一个Frame的窗口对象

--- src/com/lgm/Test.java
+++ src/com/lgm/Test.java
@@ -1,8 +1,26 @@
 package com.lgm;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * @author:李罡毛
  * @date:2021/2/4 13:13
  */
 public class Test {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setBackground(Color.lightGray);
+        frame.setResizable(false);
+        frame.setTitle("坦克大战");
+        frame.setSize(800,600);
+        frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
