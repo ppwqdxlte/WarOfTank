7939d8ffde10ba71b04543ffd4a2738b167d0c74
王昱人 <31147133+291298523@users.noreply.github.com>
2021-02-04 13:12:32 +0800

    测试

--- src/com/lb/TestLb.java
+++
@@ -1,8 +0,0 @@
-package com.lb;
-
-/**
- * @author:李罡毛
- * @date:2021/2/4 12:43
- */
-public class TestLb {
-}
--- src/com/lgm/Demo/TestDemo.java
+++
@@ -1,8 +0,0 @@
-package com.lgm.Demo;
-
-/**
- * @author:李罡毛
- * @date:2021/2/4 12:41
- */
-public class TestDemo {
-}
