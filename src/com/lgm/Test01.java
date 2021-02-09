7b21b80cfabcdd090e77c9684687673624458a76
王昱人 <31147133+291298523@users.noreply.github.com>
2021-02-09 13:49:50 +0800

    删除其它坦克的初始化部分，为网络版做准备

--- src/com/lgm/Test01.java
+++ src/com/lgm/Test01.java
@@ -9,13 +9,12 @@ public class Test01 {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
         int initTankCount = Integer.parseInt((String)PropertiesMgr.getProperty("initTankCount"));
-        //初始化电脑坦克
+        /*//初始化电脑坦克
         for (int i = 0; i < initTankCount; i++) {
             tankFrame.getTanks().add(new Tank(300+i*50,10,Dir.DOWN,tankFrame,Group.BAD));
             if (i%5 == 0)
             tankFrame.getTanks().add(new Tank(100+i*50,300,Dir.UP,tankFrame,Group.GOOD));
-        }
-
+        }*/
         new Thread(()->new Audio("audio/war1.wav").loop()).start();
 
         while (true){
