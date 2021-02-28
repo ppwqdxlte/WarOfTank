52fbf5f63ba15ee0266405f91e008c519504de1d
王昱人 <31147133+291298523@users.noreply.github.com>
2021-02-28 23:32:31 +0800

    command命令模式demo:command模式又名action、transaction事务，封装命令，结合cor实现undo...

---
+++ DesignPatterns/src/command/package-info.java
@@ -0,0 +1,13 @@
+package command;
+/**
+ *
+ *Command模式，又名Action行动\Transaction事务模式：
+ *              常用于封装命令，结合cor责任链模式实现undo
+ *
+ *
+ *          宏命令、              command和composite模式
+ *          多次undo、           command和责任链
+ *          transaction回滚：    command和 ？模式
+ *
+ *
+ */
